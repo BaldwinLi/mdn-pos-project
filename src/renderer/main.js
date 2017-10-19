55f69ac9b99a1672e1282c79ffe7dcde0858a5ec
Baldwin Li <noneasd88@126.com>
2017-10-19 21:37:00 +0800

    update for server.js
    update for server.js

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+// import lang from 'element-ui/lib/locale/lang/en'
 import {
   Row,
   Col,
@@ -14,28 +15,40 @@ import {
   Input,
   Switch
 } from 'element-ui'
+import locale from 'element-ui/lib/locale'
+// import ElementUI from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/zh-CN'
 import 'element-ui/lib/theme-default/index.css'
 import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
 import App from './App'
 import router from './router'
 import store from './store'
 
+// 设置语言
+debugger
+locale.use(lang)
+// console.log(lang)
+// locale.use(zhlang)
+// Vue.use(ElementUI, {locale: lang})
 window.Vue = Vue
-window.Vue.use(Row)
-window.Vue.use(Col)
-window.Vue.use(Menu)
-window.Vue.use(Submenu)
-window.Vue.use(MenuItem)
-window.Vue.use(MenuItemGroup)
-window.Vue.use(CollapseTransition)
-window.Vue.use(Button)
-window.Vue.use(Dialog)
-window.Vue.use(Input)
-window.Vue.use(Switch)
+// window.Vue.use(ElementUI, {zhlang})
+
+window.Vue.component(Row.name, Row)
+window.Vue.component(Col.name, Col)
+window.Vue.component(Menu.name, Menu)
+window.Vue.component(Submenu.name, Submenu)
+window.Vue.component(MenuItem.name, MenuItem)
+window.Vue.component(MenuItemGroup.name, MenuItemGroup)
+window.Vue.component(CollapseTransition.name, CollapseTransition)
+window.Vue.component(Button.name, Button)
+window.Vue.component(Dialog.name, Dialog)
+window.Vue.component(Input.name, Input)
+window.Vue.component(Switch.name, Switch)
 window.Vue.prototype.$confirm = MessageBox.confirm
 window.Vue.prototype.$alert = MessageBox.alert
 window.Vue.prototype.$message = Message
 
+window.Vue.prototype.$confirm()
 if (!process.env.IS_WEB) window.Vue.use(require('Vue-electron'))
 window.Vue.http = window.Vue.prototype.$http = axios
 window.Vue.config.productionTip = false
